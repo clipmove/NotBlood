07d03a1bbcf43ebfc7614dc901fbfd6393451eb8
tmyqlfpir <80724828+tmyqlfpir@users.noreply.github.com>
2025-03-14 09:19:55 +1000

    rednukem: always disable unsupported polymer renderer

--- source/rr/src/game.cpp
+++ source/rr/src/game.cpp
@@ -8303,6 +8303,10 @@ int app_main(int argc, char const * const * argv)
         }
     }
 #endif
+#ifdef POLYMER
+    if (glrendmode == REND_POLYMER) // polymer is unsupported
+        glrendmode = REND_POLYMOST;
+#endif
 
     G_LoadGroups(!g_noAutoLoad && !ud.setup.noautoload);
 //    flushlogwindow = 1;
