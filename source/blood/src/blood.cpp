1c1ae910032cfe73b6017cbd16de992cc1f564c8
tmyqlfpir <80724828+tmyqlfpir@users.noreply.github.com>
2025-09-22 07:22:32 +1000

    Move input polling at start of game loop

--- source/blood/src/blood.cpp
+++ source/blood/src/blood.cpp
@@ -2267,6 +2267,7 @@ RESTART:
             }
             if (numplayers == 1)
                 gBufferJitter = 0;
+            ctrlGetInput();
             if (totalclock >= gNetFifoClock && ready2send)
             {
                 do
@@ -2274,7 +2275,6 @@ RESTART:
                     if (!frameJustDrawn)
                         break;
                     frameJustDrawn = false;
-                    ctrlGetInput();
                     gNetInput = gInput;
                     gInput = {};
                     do
