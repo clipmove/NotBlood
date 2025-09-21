7c7141443e5a52cb683367bfdbdd6af126f2f974
tmyqlfpir <80724828+tmyqlfpir@users.noreply.github.com>
2025-09-21 11:49:28 +1000

    Blood: Poll input before ProcessFrame()

--- source/blood/src/blood.cpp
+++ source/blood/src/blood.cpp
@@ -1905,6 +1905,7 @@ RESTART:
                     if (!frameJustDrawn)
                         break;
                     frameJustDrawn = false;
+                    ctrlGetInput();
                     gNetInput = gInput;
                     gInput = {};
                     do
