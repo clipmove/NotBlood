3e93cd372327b4a79f1bf40509e1fec76a703c12
Martijn van Antwerpen <meelmva@hotmail.com>
2025-09-30 08:06:51 +0200

    Rednukem: Add detection for GOG version of WWII GI

--- source/rr/src/common.cpp
+++ source/rr/src/common.cpp
@@ -888,6 +888,17 @@ void G_AddSearchPaths(void)
         Bstrncpy(suffix, "/WW2GI", remaining);
         addsearchpath_user(buf, SEARCHPATH_WW2GI);
     }
+    
+    // WWII GI - GOG.com
+    bufsize = sizeof(buf);
+    if (Paths_ReadRegistryValue(R"(SOFTWARE\GOG.com\Games\1625778777)", "path", buf, &bufsize))
+    {
+        char * const suffix = buf + bufsize - 1;
+        DWORD const remaining = sizeof(buf) - bufsize;
+
+        Bstrncpy(suffix, "/WW2GI", remaining);
+        addsearchpath_user(buf, SEARCHPATH_WW2GI);
+    }
 #endif
 #endif
 #endif
