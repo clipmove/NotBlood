clipmove/NotBlood

commit 91d8dbc84b1996d8170cd41de76cc31d4482491a

Add ammo pickup scale mutator

source/blood/src/network.cpp
@@ -75,7 +75,7 @@ char gNetAddress[32];
 int gNetPort = kNetDefaultPort;
 int gNetPortLocal = -1;
 
-const short kNetVersion = 0x23C;
+const short kNetVersion = 0x23D;
 
 PKT_STARTGAME gPacketStartGame;
 

source/blood/src/player.cpp
@@ -1694,6 +1694,7 @@ char PickupItem(PLAYER *pPlayer, spritetype *pItem) {
 char PickupAmmo(PLAYER* pPlayer, spritetype* pAmmo) {
     AMMOITEMDATA* pAmmoItemData = &gAmmoItemData[pAmmo->type - kItemAmmoBase];
     int nAmmoType = pAmmoItemData->type;
+    const int nAmmoOld = pPlayer->ammoCount[nAmmoType];
 
     if (pPlayer->ammoCount[nAmmoType] >= gAmmoInfo[nAmmoType].max) return 0;
     #ifdef NOONE_EXTENSIONS
@@ -1705,6 +1706,28 @@ char PickupAmmo(PLAYER* pPlayer, spritetype* pAmmo) {
 
     if (pAmmoItemData->weaponType)  pPlayer->hasWeapon[pAmmoItemData->weaponType] = 1;
     sfxPlay3DSound(pPlayer->pSprite, 782, -1, 0);
+    if (gGameOptions.nAmmoScale && !VanillaMode())
+    {
+        const int nAmmoDiff = pPlayer->ammoCount[nAmmoType] - nAmmoOld;
+        switch (gGameOptions.nAmmoScale)
+        {
+        case 1: // 1.25x
+            pPlayer->ammoCount[nAmmoType] = ClipLow(pPlayer->ammoCount[nAmmoType] + ((nAmmoDiff>>1)>>1), 0);
+            break;
+        case 2: // 1.5x
+            pPlayer->ammoCount[nAmmoType] = ClipHigh(pPlayer->ammoCount[nAmmoType] + (nAmmoDiff>>1), gAmmoInfo[nAmmoType].max);
+            break;
+        case 3: // 0.25x
+            pPlayer->ammoCount[nAmmoType] = ClipLow(pPlayer->ammoCount[nAmmoType] - ((nAmmoDiff>>1)+(nAmmoDiff>>2)), 0);
+            break;
+        case 4: // 0.5x
+            pPlayer->ammoCount[nAmmoType] = ClipLow(pPlayer->ammoCount[nAmmoType] - (nAmmoDiff>>1), 0);
+            break;
+        case 5: // 0.75x
+            pPlayer->ammoCount[nAmmoType] = ClipLow(pPlayer->ammoCount[nAmmoType] - ((nAmmoDiff>>1)>>1), 0);
+            break;
+        }
+    }
     return 1;
 }
 

source/blood/src/blood.cpp
@@ -699,6 +699,7 @@ void StartLevel(GAMEOPTIONS *pOpt)
         pOpt->nEnemyBehavior = gEnemyBehavior;
         pOpt->bEnemyRandomTNT = gEnemyRandomTNT;
         pOpt->nWeaponsVer = gWeaponsVer;
+        pOpt->nAmmoScale = gAmmoScale;
         pOpt->bSectorBehavior = gSectorBehavior;
         pOpt->nHitscanProjectiles = gHitscanProjectiles;
         pOpt->nGoreBehavior = gGoreBehavior;
@@ -746,6 +747,7 @@ void StartLevel(GAMEOPTIONS *pOpt)
         pOpt->nEnemyBehavior = gPacketStartGame.nEnemyBehavior;
         pOpt->bEnemyRandomTNT = gPacketStartGame.bEnemyRandomTNT;
         pOpt->nWeaponsVer = gPacketStartGame.nWeaponsVer;
+        pOpt->nAmmoScale = gPacketStartGame.nAmmoScale;
         pOpt->bSectorBehavior = gPacketStartGame.bSectorBehavior;
         pOpt->nHitscanProjectiles = gPacketStartGame.nHitscanProjectiles;
         pOpt->nGoreBehavior = gPacketStartGame.nGoreBehavior;

source/blood/src/config.cpp
@@ -196,6 +196,7 @@ int gNapalmFalloff;
 int gEnemyBehavior;
 int gEnemyRandomTNT;
 int gWeaponsVer;
+int gAmmoScale;
 int gSectorBehavior;
 int gHitscanProjectiles;
 int gGoreBehavior;
@@ -592,6 +593,7 @@ void CONFIG_SetDefaults(void)
     gEnemyBehavior = 1;
     gEnemyRandomTNT = 0;
     gWeaponsVer = 0;
+    gAmmoScale = 0;
     gSectorBehavior = 0;
     gHitscanProjectiles = 0;
     gGoreBehavior = 0;
@@ -931,6 +933,7 @@ int CONFIG_ReadSetup(void)
     SCRIPT_GetNumber(scripthandle, "Game Options", "EnemyBehavior", &gEnemyBehavior);
     SCRIPT_GetNumber(scripthandle, "Game Options", "EnemyRandomTNT", &gEnemyRandomTNT);
     SCRIPT_GetNumber(scripthandle, "Game Options", "WeaponsVer", &gWeaponsVer);
+    SCRIPT_GetNumber(scripthandle, "Game Options", "AmmoScale", &gAmmoScale);
     SCRIPT_GetNumber(scripthandle, "Game Options", "SectorBehavior", &gSectorBehavior);
     SCRIPT_GetNumber(scripthandle, "Game Options", "HitscanProjectiles", &gHitscanProjectiles);
     SCRIPT_GetNumber(scripthandle, "Game Options", "GoreBehavior", &gGoreBehavior);
@@ -1238,6 +1241,7 @@ void CONFIG_WriteSetup(uint32_t flags)
     SCRIPT_PutNumber(scripthandle, "Game Options", "EnemyBehavior", gEnemyBehavior, FALSE, FALSE);
     SCRIPT_PutNumber(scripthandle, "Game Options", "EnemyRandomTNT", gEnemyRandomTNT, FALSE, FALSE);
     SCRIPT_PutNumber(scripthandle, "Game Options", "WeaponsVer", gWeaponsVer, FALSE, FALSE);
+    SCRIPT_PutNumber(scripthandle, "Game Options", "AmmoScale", gAmmoScale, FALSE, FALSE);
     SCRIPT_PutNumber(scripthandle, "Game Options", "SectorBehavior", gSectorBehavior, FALSE, FALSE);
     SCRIPT_PutNumber(scripthandle, "Game Options", "HitscanProjectiles", gHitscanProjectiles, FALSE, FALSE);
     SCRIPT_PutNumber(scripthandle, "Game Options", "GoreBehavior", gGoreBehavior, FALSE, FALSE);

source/blood/src/config.h
@@ -177,6 +177,7 @@ extern int gNapalmFalloff;
 extern int gEnemyBehavior;
 extern int gEnemyRandomTNT;
 extern int gWeaponsVer;
+extern int gAmmoScale;
 extern int gSectorBehavior;
 extern int gHitscanProjectiles;
 extern int gGoreBehavior;

source/blood/src/levels.cpp
@@ -74,7 +74,8 @@ GAMEOPTIONS gSingleGameOptions = {
     0,     // bool bNapalmFalloff;
     0,     // int nEnemyBehavior;
     0,     // bool bEnemyRandomTNT;
-    1,     // int nWeaponsVer;
+    0,     // short nWeaponsVer;
+    0,     // short nAmmoScale;
     0,     // bool bSectorBehavior;
     0,     // char nHitscanProjectiles;
     0,     // char nGoreBehavior;

source/blood/src/levels.h
@@ -66,7 +66,8 @@ struct GAMEOPTIONS {
     bool bNapalmFalloff;
     int nEnemyBehavior;
     bool bEnemyRandomTNT;
-    int nWeaponsVer;
+    short nWeaponsVer;
+    short nAmmoScale;
     bool bSectorBehavior;
     char nHitscanProjectiles;
     char nGoreBehavior;

source/blood/src/loadsave.cpp
@@ -280,6 +280,7 @@ void LoadSave::LoadGame(char *pzFile)
         gGameOptions.nEnemyBehavior = gEnemyBehavior;
         gGameOptions.bEnemyRandomTNT = gEnemyRandomTNT;
         gGameOptions.nWeaponsVer = gWeaponsVer;
+        gGameOptions.nAmmoScale = gAmmoScale;
         gGameOptions.bSectorBehavior = gSectorBehavior;
         gGameOptions.nHitscanProjectiles = gHitscanProjectiles;
         gGameOptions.nGoreBehavior = gGoreBehavior;

commit 9aabf26ef1112db6c4eb9f147f8710a956c051d6

Fix ammo scale bug introduced in 91d8dbc84b1996d8170cd41de76cc31d4482491a

source/blood/src/network.cpp
@@ -75,7 +75,7 @@ char gNetAddress[48];
 int gNetPort = kNetDefaultPort;
 int gNetPortLocal = -1;
 
-const short kNetVersion = 0x240;
+const short kNetVersion = 0x241;
 
 PKT_STARTGAME gPacketStartGame;
 

source/blood/src/player.cpp
@@ -1694,37 +1694,38 @@ char PickupItem(PLAYER *pPlayer, spritetype *pItem) {
 char PickupAmmo(PLAYER* pPlayer, spritetype* pAmmo) {
     AMMOITEMDATA* pAmmoItemData = &gAmmoItemData[pAmmo->type - kItemAmmoBase];
     int nAmmoType = pAmmoItemData->type;
-    const int nAmmoOld = pPlayer->ammoCount[nAmmoType];
+    const int oldAmmoCount = pPlayer->ammoCount[nAmmoType];
+    int nAmmoAdded = 0;
 
     if (pPlayer->ammoCount[nAmmoType] >= gAmmoInfo[nAmmoType].max) return 0;
     #ifdef NOONE_EXTENSIONS
     else if (gModernMap && pAmmo->extra >= 0 && xsprite[pAmmo->extra].data1 > 0) // allow custom amount for item
-        pPlayer->ammoCount[nAmmoType] = ClipHigh(pPlayer->ammoCount[nAmmoType] + xsprite[pAmmo->extra].data1, gAmmoInfo[nAmmoType].max);
+        pPlayer->ammoCount[nAmmoType] = ClipHigh(pPlayer->ammoCount[nAmmoType] + xsprite[pAmmo->extra].data1, gAmmoInfo[nAmmoType].max), nAmmoAdded = xsprite[pAmmo->extra].data1;
     #endif
     else
-        pPlayer->ammoCount[nAmmoType] = ClipHigh(pPlayer->ammoCount[nAmmoType]+pAmmoItemData->count, gAmmoInfo[nAmmoType].max);
+        pPlayer->ammoCount[nAmmoType] = ClipHigh(pPlayer->ammoCount[nAmmoType]+pAmmoItemData->count, gAmmoInfo[nAmmoType].max), nAmmoAdded = pAmmoItemData->count;
 
     if (pAmmoItemData->weaponType)  pPlayer->hasWeapon[pAmmoItemData->weaponType] = 1;
     sfxPlay3DSound(pPlayer->pSprite, 782, -1, 0);
     if (gGameOptions.nAmmoScale && !VanillaMode())
     {
-        const int nAmmoDiff = pPlayer->ammoCount[nAmmoType] - nAmmoOld;
+        pPlayer->ammoCount[nAmmoType] = oldAmmoCount;
         switch (gGameOptions.nAmmoScale)
         {
         case 3: // 0.25x
-            pPlayer->ammoCount[nAmmoType] = ClipLow(pPlayer->ammoCount[nAmmoType] - ((nAmmoDiff>>1)+(nAmmoDiff>>2)), 0);
+            pPlayer->ammoCount[nAmmoType] = ClipHigh(pPlayer->ammoCount[nAmmoType] + (nAmmoAdded>>2), gAmmoInfo[nAmmoType].max);
             break;
         case 4: // 0.5x
-            pPlayer->ammoCount[nAmmoType] = ClipLow(pPlayer->ammoCount[nAmmoType] - (nAmmoDiff>>1), 0);
+            pPlayer->ammoCount[nAmmoType] = ClipHigh(pPlayer->ammoCount[nAmmoType] + (nAmmoAdded>>1), gAmmoInfo[nAmmoType].max);
             break;
         case 5: // 0.75x
-            pPlayer->ammoCount[nAmmoType] = ClipLow(pPlayer->ammoCount[nAmmoType] - ((nAmmoDiff>>1)>>1), 0);
+            pPlayer->ammoCount[nAmmoType] = ClipHigh(pPlayer->ammoCount[nAmmoType] + ((nAmmoAdded>>1)+(nAmmoAdded>>2)), gAmmoInfo[nAmmoType].max);
             break;
         case 1: // 1.25x
-            pPlayer->ammoCount[nAmmoType] = ClipHigh(pPlayer->ammoCount[nAmmoType] + ((nAmmoDiff>>1)>>1), gAmmoInfo[nAmmoType].max);
+            pPlayer->ammoCount[nAmmoType] = ClipHigh(pPlayer->ammoCount[nAmmoType] + nAmmoAdded+(nAmmoAdded>>2), gAmmoInfo[nAmmoType].max);
             break;
         case 2: // 1.5x
-            pPlayer->ammoCount[nAmmoType] = ClipHigh(pPlayer->ammoCount[nAmmoType] + (nAmmoDiff>>1), gAmmoInfo[nAmmoType].max);
+            pPlayer->ammoCount[nAmmoType] = ClipHigh(pPlayer->ammoCount[nAmmoType] + nAmmoAdded+(nAmmoAdded>>1), gAmmoInfo[nAmmoType].max);
             break;
         }
     }